873501de1f3b185b5dd6940058b0fe4b7598e80c
NaHid <nahid.xsr@gmail.com>
2025-03-22 05:35:47 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function generateImage() {
-    let prompt = document.getElementById("prompt").value;
-    let imageUrl = `https://rximagegen.r1y4zrx.workers.dev/?prompt=${encodeURIComponent(prompt)}`;
-    
-    let imageContainer = document.getElementById("image-container");
-    imageContainer.innerHTML = `<img src="${imageUrl}" alt="Generated Image">`;
-}
